38df67ad85b9cddcabeb20f7e826f53ae49305e7
nawodyaishan <nawodyain@gmail.com>
2022-10-28 22:46:26 +0530

    Add check for ship

---
+++ test/main_test.js
@@ -0,0 +1,8 @@
+var expect = require(`chai`).expect;
+
+// Test Suite
+describe(`Mocha`, function () {
+    it(`should run ove tests using npm`, function () {
+        expect(`true`).to.be.ok;
+    });
+});
---
+++ test/ship_test.js
@@ -0,0 +1,9 @@
+var expect = require(`chai`).expect;
+
+describe(`checkForShip`, function (){
+    var checkForShip = require(`../game_logic/ship_methods`).checkForShips;
+
+    it('should correctly report no ship at the given players coordinate ', function () {
+        
+    });
+})
